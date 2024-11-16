03f362a0802d4dd02293b1d7431bdca239ae967c
csabimvp <kellercsabii@gmail.com>
2024-11-16 13:18:26 +0000

    SQL database, schema and tables created..

--- sql/schema/schema.sql
+++ sql/schema/schema.sql
@@ -8,10 +8,10 @@ CREATE SCHEMA rstop500 AUTHORIZATION csabimvp;
 
 CREATE TABLE
     IF NOT EXISTS rstop500.tracks (
-        track_id VARCHAR NOT NULL PRIMARY KEY,
-        track_name VARCHAR NOT NULL,
+        track_id VARCHAR PRIMARY KEY,
+        track_name VARCHAR,
         artist_ids VARCHAR ARRAY,
-        rs_rank INTEGER NOT NULL,
+        rs_rank INTEGER,
         is_explicit BOOLEAN,
         popularity NUMERIC,
         duration NUMERIC,
@@ -23,8 +23,8 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS rstop500.artists (
-        artist_id VARCHAR NOT NULL PRIMARY KEY,
-        artist_name VARCHAR NOT NULL,
+        artist_id VARCHAR PRIMARY KEY,
+        artist_name VARCHAR,
         genres VARCHAR ARRAY,
         popularity NUMERIC,
         total_followers NUMERIC,
@@ -34,20 +34,23 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS rstop500.albums (
-        album_id VARCHAR NOT NULL PRIMARY KEY,
-        album_name VARCHAR NOT NULL,
-        rs_rank INTEGER NOT NULL,
+        album_id VARCHAR PRIMARY KEY,
+        album_name VARCHAR,
+        rs_rank INTEGER,
         genres VARCHAR ARRAY,
         popularity NUMERIC,
         total_tracks NUMERIC,
         external_url VARCHAR,
         label VARCHAR
         -- artist_id VARCHAR,
-        -- track_id VARCHAR NOT NULL REFERENCES rstop500.tracks,
+        -- track_id VARCHAR REFERENCES rstop500.tracks,
     );
 
 ALTER TABLE rstop500.tracks ADD album_id VARCHAR REFERENCES rstop500.albums;
 
-ALTER TABLE rstop500.albums ADD artist_id VARCHAR NOT NULL REFERENCES rstop500.artists;
+ALTER TABLE rstop500.albums ADD artist_id VARCHAR REFERENCES rstop500.artists;
 
-ALTER TABLE rstop500.albums ADD track_id VARCHAR NOT NULL REFERENCES rstop500.tracks;
+ALTER TABLE rstop500.albums ADD track_id VARCHAR REFERENCES rstop500.tracks;
+
+-- \q
+-- exit
